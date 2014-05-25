bbf09bbb28f149ffc5482371908ef0d40b366a9c
Vitmonk <tmb@live.de>
2014-05-25 14:26:00 +0200

    Convertierung zwischen chars und bits hinzugefügt. ('A' zu "01000001" und umgekehrt)

--- SCModel.cpp
+++ SCModel.cpp
@@ -6,6 +6,10 @@
  */
 
 #include "SCModel.h"
+#include <assert.h>
+#include <string>
+#include <bitset>
+#include <cstdlib>
 
 SCModel::SCModel() {
 }
@@ -13,3 +17,30 @@ SCModel::SCModel() {
 SCModel::~SCModel() {
 }
 
+/*
+ * Returns an bit representation of an unsigned char as string.
+ * Example: 'A' is "01000001"
+ */
+std::string SCModel::charToBits(const unsigned char& c) const {
+    assert(c);
+    std::string bits = "";
+    for (int i = 7; i >= 0; i--) {
+        bits.append((c & (1 << i)) ? "1" : "0");
+    }
+    return bits;
+};
+
+/*
+ * Returns an unsigned char of given bit representation for an unsigned char.
+ * Example: "01000001" is 'A'
+ */
+unsigned char SCModel::bitsToChar(const std::string& bits) const {
+    assert(bits.size() == 8);
+    unsigned char c = '$';
+    std::bitset<8> bitsArray(bits);
+    unsigned int asciiPos = bitsArray.to_ulong();
+    if (asciiPos <= CHAR_MAX) {
+        c = static_cast<unsigned char> (asciiPos);
+    }
+    return c;
+};
--- SCModel.h
+++ SCModel.h
@@ -8,12 +8,15 @@
 #ifndef SCMODEL_H
 #define	SCMODEL_H
 
+#include <string>
+
 class SCModel {
 public:
     SCModel();
     virtual ~SCModel();
 private:
-
+    std::string charToBits(const unsigned char& c) const;
+    unsigned char bitsToChar(const std::string& bits) const;
 };
 
 #endif	/* SCMODEL_H */
